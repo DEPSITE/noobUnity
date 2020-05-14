8146ba0204f89da805fb012a781ad6768e613324
DEPSITE <62496275+DEPSITE@users.noreply.github.com>
2020-05-14 20:21:21 +0900

    구분선
    노트까지

---
+++ Rhythmgame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != null) Destroy(gameObject);
+    }
+    public float noteSpeed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
