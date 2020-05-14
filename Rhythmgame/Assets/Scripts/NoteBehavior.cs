89b4bf050e9b9c01b9becccdc704eeab890d6106
DEPSITE <62496275+DEPSITE@users.noreply.github.com>
2020-05-14 21:33:39 +0900

    오브젝트풀링

--- Rhythmgame/Assets/Scripts/NoteBehavior.cs
+++ Rhythmgame/Assets/Scripts/NoteBehavior.cs
@@ -18,13 +18,18 @@ public class NoteBehavior : MonoBehaviour
     }
 
     // Update is called once per frame
+    public void Initialize()
+    {
+        judge = GameManager.judges.NONE;
+    }
     void Update()
+       
     {
         transform.Translate(Vector3.down * GameManager.instance.noteSpeed);
         if(Input.GetKey(keyCode))
         {
             Debug.Log(judge);
-            if (judge != GameManager.judges.NONE) Destroy(gameObject);
+            if (judge != GameManager.judges.NONE) gameObject.SetActive(false);
         }
     }
     private void OnTriggerEnter2D(Collider2D other)
@@ -44,7 +49,7 @@ public class NoteBehavior : MonoBehaviour
         else if (other.gameObject.tag == "Miss Line")
         {
             judge = GameManager.judges.MISS;
-            Destroy(gameObject);
+            gameObject.SetActive(false);
         }
         Debug.Log(judge);
     }
