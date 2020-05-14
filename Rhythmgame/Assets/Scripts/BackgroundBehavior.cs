400d2fdef798dfd8e1de8bdecf5fef7f78abdf21
DEPSITE <62496275+DEPSITE@users.noreply.github.com>
2020-05-14 20:08:13 +0900

    게임화면구성
    1강

---
+++ Rhythmgame/Assets/Scripts/BackgroundBehavior.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class BackgroundBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject gameBackground;
+    private SpriteRenderer gameBackgroundSpriteRenderer;
+
+
+    void Start()
+    {
+        gameBackgroundSpriteRenderer=gameBackground.GetComponent<SpriteRenderer>();
+        StartCoroutine(FadeOut(gameBackgroundSpriteRenderer, 0.005f));
+    }
+
+    IEnumerator FadeOut(SpriteRenderer spriteRenderer, float amount)
+    {
+        Color color = spriteRenderer.color;
+        while (color.a > 0.0f)
+        {
+            color.a -= amount;
+            spriteRenderer.color = color;
+            yield return new WaitForSeconds(amount);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
